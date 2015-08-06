9c9d93b1c817bf31630beabe0d1ede96a8cee74b
Ryan Smith <0ryansmith1994@gmail.com>
2015-08-06 15:13:10 +0100

    Fixes UserRegistered.

--- src/Events/UserRegistered.php
+++ src/Events/UserRegistered.php
@@ -13,12 +13,6 @@ class UserRegistered extends Event {
             'user_id' => $opts['relateduser']->id,
             'user_url' => $opts['relateduser']->url,
             'user_name' => $opts['relateduser']->username,
-            'course_url' => $opts['course']->url,
-            'course_name' => $opts['course']->fullname ?: 'A Moodle course',
-            'course_description' => $opts['course']->summary ?: 'A Moodle course',
-            'course_type' => static::$xapi_type.$opts['course']->type,
-            'course_ext' => $opts['course'],
-            'course_ext_key' => 'http://lrs.learninglocker.net/define/extensions/moodle_course',
         ]);
     }
 }
